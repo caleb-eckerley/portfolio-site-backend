a2278070f061b04dcb46aa6515f8a5d8d2445595
Caleb <cjeckerley@gmail.com>
2024-01-26 13:33:13 -0500

    Removed config from .gitignore

---
+++ config/corsOptions.js
@@ -0,0 +1,15 @@
+const whitelist = process.env.CORS_WHITELIST.split(" ");
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+module.exports = corsOptions;
